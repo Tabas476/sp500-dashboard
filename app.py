0c39f044b4c2f9fe96a17d2ffcfa89b7ca85d6fa
Tabas476 <jcsanchezdecordova@icloud.com>
2026-05-08 08:58:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ cols = st.columns(3)
 for i, (name, ticker) in enumerate(indexes.items()):
     try:
         q = get_quote(ticker)
-        st.write(q)
-        change = q.get("changesPercentage", 0)
+        change = q.get("changePercentage", 0)
         cols[i].metric(
             label=name,
             value=f"{q.get('price', 'N/A')}",
